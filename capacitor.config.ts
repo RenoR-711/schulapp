36a7c7bb24a177e9ae0c2ae4388fb1abe470463d
RenoR <renu711@hotmail.de>
2025-11-19 16:28:04 +0100

    add new files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.iss.menueplan',
+  appName: 'iss-menue-plan',
+  webDir: 'dist'
+};
+
+export default config;
