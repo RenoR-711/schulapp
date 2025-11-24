92fbac1e2c4789ec8c519bfbb436c19d63a2c230
RenoR <renu711@hotmail.de>
2025-11-24 09:51:29 +0100

    delete

--- src/main_backup.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import { router } from './router'; // ← Router laden
-import './style.css'; // Tailwind + global styles
-import './assets/main.css';
-
-const app = createApp(App);
-app.use(router);                     // ← Router aktivieren
-app.mount('#app');
